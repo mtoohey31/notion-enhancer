11514ee7f065df31ea02503e9f4115037b68dc74
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-02-10 23:26:55 -0500

    Added hard-coded math font mod for fira math

---
+++ mods/fira-math/mod.js
@@ -0,0 +1,14 @@
+/*
+ * fira math
+ * (c) 2020 mtoohey31
+ * under the MIT license
+ */
+
+module.exports = {
+  id: 'a97311e4-5142-430e-bde4-a80e73367814',
+  name: 'fira math',
+  tags: ['theme'],
+  desc: 'set math fonts to fira math',
+  version: '0.1.0',
+  author: 'mtoohey31',
+};
