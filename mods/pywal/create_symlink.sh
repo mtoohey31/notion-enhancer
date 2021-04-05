2d49b36d3beb08a792add2e10a1f72d54ee78780
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-04-04 20:55:39 -0400

    Added pywal theme

---
+++ mods/pywal/create_symlink.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+ln -s $HOME/.cache/wal/colors.notion.css ./colors.css
---
+++ mods/pywal/mod.js
@@ -0,0 +1,22 @@
+/*
+ * pywal
+ * adapted by:
+ * @mtoohey31
+ * adapted from:
+ * dracula
+ * (c) 2020 @mimishahzad386#5651
+ * (c) 2020 dracula
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+  id: 'df1aba5d-f110-405f-b1df-6cad4b1e4d71',
+  tags: ['theme'],
+  name: 'pywal',
+  desc:
+    'a theme to pull colors from the cached pywal color scheme. ',
+  version: '0.1.0',
+  author: 'mtoohey31',
+};
